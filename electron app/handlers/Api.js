4e5cc3baef5f94f3b49926851fae5f273e75dfd7
Uiop Pilon <Circutrider21@outlook.com>
2020-01-09 19:49:28 -0600

    Overhauled and almost finished the api and backend. ready for 0.6.5

--- electron app/handlers/Api.js
+++ electron app/handlers/Api.js
@@ -1,11 +1,35 @@
+let fs = require('fs')
 var request = new XMLHttpRequest()
 
 
 var city = document.getElementById("city").value;
-// Open a new connection, using the GET request on the URL endpoint
-var params = JSON.stringify({ city: });
+var state = document.getElementById("state").value;
+var country = document.getElementById("country").value;
 
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+if(fs.existsSync(filename)) {
+      let data = fs.readFileSync(filename, 'utf8').split('\n')
+
+      data.forEach((contact, index) => {
+         let [ name, email ] = contact.split(',')
+         addEntry(name, email)
+      })
+
+   } else {
+      console.log("File Doesn\'t Exist. Creating new file.")
+      fs.writeFile(filename, '', (err) => {
+         if(err)
+            console.log(err)
+      }
+  }
+// Open a new connection, using the GET request on the URL endpoint
+var params = JSON.stringify({
+  city: city,
+  state: state,
+  country:country,
+  method: 2
+});
+
+request.open('POST', 'https://ghibliapi.herokuapp.com/films', true)
 
 request.onload = function() {
   // Begin accessing JSON data here
@@ -15,7 +39,6 @@ request.onload = function() {
   var asr = salat.data.Asr
   var maghirb = salat.data.Maghirb
   var isha = salat.data.Isha
-
 }
 
 // Send request
