9e0f8f080b575529ddeb1e95b1ac7ea10c1efe07
circutrider21 <57774961+circutrider21@users.noreply.github.com>
2020-02-27 21:54:22 +0000

    Half Done in 6th hour

--- electron app/handlers/main.js
+++ electron app/handlers/main.js
@@ -1,9 +1,9 @@
 $("#submit").click(function(){
   var city = $("#city").text();
   var country = $("#country").text();
-  var url = "";
+  var url = "http://api.aladhan.com/v1/timingsByCity?city=" + city + "&country=" + country + "&method=2";
   $.get(url, function(data, status){
     var info = JSON.parse(data);
-    var dhuhr = info.data.timings.dhuhr;
+    var prayers = [info.data.timings.dhuhr];
   });
 });
